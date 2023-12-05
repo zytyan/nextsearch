2579974536c0bc0f0bc08c411c8abff11a7faa33
zytyan <58136520+zytyan@users.noreply.github.com>
2023-12-05 23:04:35 +0800

    修复next export 引用telegram.js发生cors问题 删除 .idea 文件夹

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,15 +1,14 @@
 import {Head, Html, Main, NextScript} from 'next/document'
-import Script from "next/script";
-
 
 export default function Document() {
     return (
         <Html lang="en">
-            <Head/>
+            <Head>
+            <script src={"https://telegram.org/js/telegram-web-app.js"}
+                    data-nscript="beforeInteractive" defer={true}/>
+            </Head>
             <body>
             <Main/>
-            <Script src={"https://telegram.org/js/telegram-web-app.js"}
-                    strategy="beforeInteractive"/>
             <NextScript/>
             </body>
         </Html>
